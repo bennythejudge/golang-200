f0e66fcb932898aa41cf64dfb6be9013f555cb11
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2019-03-12 12:58:58 +0100

    Solutions

--- statistics/statistics.go
+++ statistics/statistics.go
@@ -1,6 +1,8 @@
 package statistics
 
 import (
+	"fmt"
+	logger "github.com/sirupsen/logrus"
 	"time"
 )
 
@@ -11,34 +13,63 @@ const (
 // TODO define a Statistics struct with an uint8 chan, an uint32 counter, a start time and logging period duration
 // Statistics is the worker to persist the request statistics
 type Statistics struct {
+	statistics    chan uint8
+	counter       uint32
+	start         time.Time
+	loggingPeriod time.Duration
 }
 
 // NewStatistics creates a new statistics structure and launches its worker routine
 func NewStatistics(loggingPeriod time.Duration) *Statistics {
 	// TODO build a new Statistics object with a sized channel, initialized counter and start time
 	// TODO and logging period as param
+	sw := Statistics{
+		statistics:    make(chan uint8, statisticsChannelSize),
+		counter:       0,
+		start:         time.Now(),
+		loggingPeriod: loggingPeriod,
+	}
 
 	// TODO launch the run in a separate Go routine in background
+	go sw.run()
 
 	// TODO return the initialized and started object
-	return nil
+	return &sw
 }
 
 // PlusOne is used to send a statistics hit increment
 func (sw *Statistics) PlusOne() {
 	// TODO push a hit in the statistics channel
+	sw.statistics <- uint8(1) // need to make sure the value has the right type
 }
 
 func (sw *Statistics) run() {
 	// TODO build a new time Ticker from the logging period
+	ticker := time.NewTicker(sw.loggingPeriod)
 
 	// TODO build a infinite loop and the channel selection inside
+	for {
+		select {
+		// TODO build a first select case from the statistics channel
+		// TODO add the hit count to the counter and log it as debug level
+		case stat := <-sw.statistics:
+			logger.
+				WithField("stat", stat).
+				Debug("new count received")
 
-	// TODO build a first select case from the statistics channel
-	// TODO add the hit count to the counter and log it as debug level
-
-	// TODO build a second case on the time Ticker chan
-	// TODO retrieve the elapsed time since start
-	// TODO log the hit/sec rate
-	// TODO reset the counter and the start time
+		// TODO build a second case on the time Ticker chan
+		case <-ticker.C:
+			// TODO retrieve the elapsed time since start
+			elapsed := time.Since(sw.start)
+			// TODO log the hit/sec rate
+			logger.
+				WithField("elapsed time", elapsed).
+				WithField("count", sw.counter).
+				Warn("monitoring request")
+			// TODO reset the counter and the start time
+			sw.counter = 0
+			sw.start = time.Now()
+			fmt.Println("inside the run")
+		}
+	}
 }
--- statistics/statistics_test.go
+++ statistics/statistics_test.go
@@ -6,14 +6,35 @@ import (
 )
 
 const (
-	statPeriod = 500 * time.Millisecond
+	statPeriod    = 500 * time.Millisecond
+	expectedCount = 2 // what is this value?
+	emptyCount    = 0
 )
 
 func TestStatistics(t *testing.T) {
 
-	//statistics := NewStatistics(2 * statPeriod)
+	statistics := NewStatistics(2 * statPeriod)
 
 	//TODO: implement this test to test the statistics middleware (test the value of its counter)
+	go func() {
+		statistics.PlusOne()
+	}()
+
+	go func() {
+		statistics.PlusOne()
+	}()
+
 	//TODO: you will need time.Sleep(statPeriod) because of the statistics ticker
+	time.Sleep(statPeriod)
+
+	if statistics.counter != expectedCount {
+		t.Errorf("stats count %d differs from expected %d", statistics.counter, expectedCount)
+	}
+
+	time.Sleep(2 * statPeriod)
+
+	if statistics.counter != emptyCount {
+		t.Errorf("stats counter %d differs from expected emptyCount %d", statistics.counter, emptyCount)
+	}
 
 }
